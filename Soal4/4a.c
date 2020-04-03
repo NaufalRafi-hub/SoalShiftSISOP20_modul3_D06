f8c158630c79d0d3b3137c4b9aad8d28f18522a0
Naufal Rafi Akbar <58022016+NaufalRafi-hub@users.noreply.github.com>
2020-04-03 13:03:30 +0700

    update fix 4a harusnya

--- Soal4/4a.c
+++ Soal4/4a.c
@@ -7,23 +7,35 @@
 #include <unistd.h>
 
 int main() {
-    int inputa[4][2], inputb[2][5], output[4][5], *p, sum = 0, out;
+    int inputa[4][2], inputb[2][5], output[4][5], *shm_ptr, sum = 0;
 
-    key_t key = 1234;
-    int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-    p = shmat(shmid, NULL, 0);
-
+    	key_t kunci = 1234;
+    	//shmget(key_t key, size_t size, int shmflg);
+    	int shmid = shmget(kunci, sizeof(int), IPC_CREAT | 0666);
+	//error handling
+	if (shmid < 0) {
+     		printf("*** shmget error (server) ***\n");
+     		exit(1);
+	}
+   	shm_ptr = shmat(shmid, NULL, 0);
+	//error handling
+	if (*shm_ptr == -1) {
+    		printf("*** shmat error (server) ***\n");
+     		exit(1);
+	}
+	//input mariks a
     	for (int i = 0; i < 4; i++){
     		for (int j = 0; j < 2; j++){
     	    		inputa[i][j]=1;
 		}
 	}
+	//input matriks b
 	for (int k = 0; k < 2; k++){
     		for (int l = 0; l < 5; l++){
     	    		inputb[k][l]=1;
 		}
 	}
-
+	//untuk menghitung perkalian matriks
 	for (int c = 0; c < 4; c++) {
 		for (int d = 0; d < 5; d++) {
         		for (int x = 0; x < 2; x++) {
@@ -34,17 +46,17 @@ int main() {
         	sum = 0;
 		}
     	}
+	//untuk menprint hasil perkalian
 	for (int w = 0; w < 4; w++) {
         	for (int s = 0; s < 5; s++) {
-            		*p = output[w][s];
+            		*shm_ptr = output[w][s];
            		sleep(1);
-           		printf("%d\n", *p);
+           		printf("%d\n", *shm_ptr);
         	}
     	}
 
-    shmdt(p);
+    shmdt(shm_ptr);
     shmctl(shmid, IPC_RMID, NULL);
     
     return 0;
 }
-
